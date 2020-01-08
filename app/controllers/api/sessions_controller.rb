6a20b6d4710bafc3c6d14995c59d54884c801e3d
Lex Logan <alexie.henrietta@gmail.com>
2020-01-07 16:20:44 -0800

    add heroku compatibility

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::SessionsController < ApplicationController
 
   def create
-    # debugger
     @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
     if @user.nil?
       render :json => ['Invalid username or password.'], status: 418
