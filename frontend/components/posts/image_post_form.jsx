2604488acd9111bdb7d816a6ce35d03609fa6788
Lex Logan <alexie.henrietta@gmail.com>
2020-03-11 13:14:53 -0700

    add css fix

---
+++ frontend/components/posts/image_post_form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ImagePostForm = props => {
+
+
+  return (
+  
+
+      <div className="post-body-container">
+
+       <h1>hiya</h1>
+
+      </div>
+
+    
+  )
+}
+
+export default ImagePostForm;
---
+++ frontend/components/posts/link_post_form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const LinkPostForm = props => {
+
+
+  return (
+
+
+    <div className="post-body-container">
+
+      <h1>hiya</h1>
+
+    </div>
+
+
+  )
+}
+
+export default LinkPostForm;
