85c8c772eafa310df3305d39bc65b2626944dfb2
Lex Logan <alexie.henrietta@gmail.com>
2020-03-13 17:33:18 -0700

    css fix

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -3,6 +3,8 @@ import { closeModal } from '../../actions/modal';
 import { connect } from 'react-redux';
 import LoginContainer from '../session/login_container';
 import SignupContainer from '../session/signup_container';
+import ModalLogo from '../../../app/assets/images/cattit_logos/cattit_logo.svg'
+
 
 function Modal({ modal, closeModal }) {
   if (!modal) {
@@ -29,11 +31,7 @@ function Modal({ modal, closeModal }) {
           {
             (modal === 'login') ? (
         <div className="modal-logo">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="login-logo">
-            <g><circle fill="#FF4500" cx="10" cy="10" r="10">
-            </circle>
-              <path fill="#FFF" d="M16.67,10A1.46,1.46,0,0,0,14.2,9a7.12,7.12,0,0,0-3.85-1.23L11,4.65,13.14,5.1a1,1,0,1,0,.13-0.61L10.82,4a0.31,0.31,0,0,0-.37.24L9.71,7.71a7.14,7.14,0,0,0-3.9,1.23A1.46,1.46,0,1,0,4.2,11.33a2.87,2.87,0,0,0,0,.44c0,2.24,2.61,4.06,5.83,4.06s5.83-1.82,5.83-4.06a2.87,2.87,0,0,0,0-.44A1.46,1.46,0,0,0,16.67,10Zm-10,1a1,1,0,1,1,1,1A1,1,0,0,1,6.67,11Zm5.81,2.75a3.84,3.84,0,0,1-2.47.77,3.84,3.84,0,0,1-2.47-.77,0.27,0.27,0,0,1,.38-0.38A3.27,3.27,0,0,0,10,14a3.28,3.28,0,0,0,2.09-.61A0.27,0.27,0,1,1,12.48,13.79Zm-0.18-1.71a1,1,0,1,1,1-1A1,1,0,0,1,12.29,12.08Z"></path>
-            </g></svg>
+          <img className="login-logo" src={ModalLogo} />
         </div>  
         ) : (null) }
         <div className="session-container" onClick={e => e.stopPropagation()}>
