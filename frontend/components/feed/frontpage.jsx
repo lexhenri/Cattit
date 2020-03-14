25167c80f7cb726e283c6294edaed0dd9c40a460
Lex Logan <alexie.henrietta@gmail.com>
2020-03-13 18:25:41 -0700

    add feed as an all subcattit

--- frontend/components/feed/frontpage.jsx
+++ frontend/components/feed/frontpage.jsx
@@ -18,25 +18,45 @@ class Frontpage extends React.Component {
 
   render(){
     return (
-      <div className="subcattit-container">
-        <div className="subcat-feed-container">
-          {/* <div className="mini-submit">
-            {
-              (this.props.currentUser !== undefined) ?
-                (<Link to={{ pathname: `/mew/${this.props.subcattit}/submit` }}>
-                  <input className="mini-input" placeholder="Create Post" />
-                </Link>
-                ) : (<a onClick={() => this.props.openModal('login')} >
-                  <input className="mini-input" placeholder="Create Post" />
-                </a>)
-            }
-          </div> */}
+
+      <div>
+        <div className="top-banner">
+          {/* <img src={this.props.subcattitInfo.bannerUrl} /> */}
+        </div>
+        <div className='desc-banner'>
+          <div className="header-content">
+            {/* <img src={this.props.subcattitInfo.iconUrl} className="header-pic" /> */}
+            <div className="text-container">
+              <h1 className="header-title">all</h1>
+              <h2 className='subcat-title'>cattit/all</h2>
+            </div>
+            {/* <button className="follow-btn"><span>Join</span></button> */}
+          </div>
+        </div>
+
+        <div className="subcattit-container">
+          <div className="subcat-feed-container">
+            {/* <div className="mini-submit">
+              {
+                (this.props.currentUser !== undefined) ?
+                  (<Link to={{ pathname: `/mew/${this.props.subcattit}/submit` }}>
+                    <input className="mini-input" placeholder="Create Post" />
+                  </Link>
+                  ) : (<a onClick={() => this.props.openModal('login')} >
+                    <input className="mini-input" placeholder="Create Post" />
+                  </a>)
+              }
+            </div> */}
             <AllPostContainer />
           </div>
           <div className="sidebar-container">
             <Suggested />
-          </div>
-          </div>
+           </div>
+          {/* <SubSidebar subcattit={this.props.subcattit} subcattitInfo={this.props.subcattitInfo} page={"subcattit"} currentUser={this.props.currentUser} openModal={this.props.openModal} /> */}
+
+        </div>
+      </div>
+   
   )
   }
 
